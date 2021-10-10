9d8ff8ff6c4e3f00ed42f8f6f412e0d9f7eba2a1
Your Name <advmenendez92@gmail.com>
2021-10-10 17:30:40 +0200

    second commit

--- Iteration1.js
+++ Iteration1.js
@@ -1,12 +1,15 @@
 /* 1.1 Basandote en el array siguiente, crea una lista ul > li 
 dinámicamente en el html que imprima cada uno de los paises. */
 
-const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
-let divs = document.querySelector(".div1")
+//const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+let divs = document.querySelector(".divOne")
 
 
 
 //1.2 Elimina el elemento que tenga la clase .fn-remove-me.
+const removeP = document.querySelector('.fn-remove-me')
+let body = document.querySelector(".divTwo")
+body.removeChild(removeP)
 
 /* 1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
 en el div de html con el atributo data-function="printHere". */
@@ -14,13 +17,13 @@ const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
 
 /* 1.4 Crea dinamicamente en el html una lista de div que contenga un elemento 
 h4 para el titulo y otro elemento img para la imagen. */
-const countries = [
+/* const countries = [
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1'}, 
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2'},
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3'},
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4'},
 	{title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5'}
-];
+]; */
 
 /* 1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último 
 elemento de la lista. */
